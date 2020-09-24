28d10318324e2eb0125280a9683e7c1ef9c084c2
Wizard Hat <Nlarsen23.student@gmail.com>
2020-09-24 17:34:16 +0000

    Changed setup.py repo url

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name='mcserverapi',
-    version='1.0.3',
+    version='1.2.0',
     packages=['mcserverapi'],
-    url='https://github.com/TeamNightSky/MCServerWrapper',
+    url='https://github.com/TeamNightSky/MCServerAPI',
     license='GNU License',
     author='Pixymon',
     author_email='nlarsen23.student@gmail.com',
